512116ba50618bbc254a918f6e2f2129205ddb31
Emery Jones <140751590+mtn-laurel@users.noreply.github.com.>
2025-12-30 15:41:27 -0600

    Setup backend package structure and test controller

---
+++ backend/src/main/java/com/example/secretsanta/controller/HellWorldController.java
@@ -0,0 +1,14 @@
+package com.example.secretsanta.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HellWorldController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Secret Santa!";
+    }
+    
+}
